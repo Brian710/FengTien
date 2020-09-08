74e35f515d383d867aee35f4da9e44a9a2726b2c
Brian710 <mmm850710@gmail.com>
2020-09-08 10:00:24 +0800

    CleanWasteAdd

---
+++ FengTienPro/Assets/Scripts/Interactable/08FeedMeds/BowlPillController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class BowlPillController : IObjControllerBase
+{
+    [SerializeField] private Animator Anim;
+    [SerializeField] private GameObject Powder;
+    [SerializeField] private Goal.Type type;
+    [SerializeField] private Collider colli;
+    private int GrindNum;
+ 
+
+    public override void Awake()
+    {
+        base.Awake();
+        goalType = type;
+    }
+    public override void Start()
+    {
+        if (Anim == null) Anim = ChildObj.GetComponent<Animator>();
+        base.Start();
+    }
+
+    public override void SetChildObjActive(bool value)
+    {
+        if (value)
+        {
+            SetWaitingState();
+        }
+        else
+        {
+            ChildObj.SetActive(false);
+            Powder.SetActive(false);
+        }
+    }
+    protected override void SetWaitingState()
+    {
+        base.SetWaitingState();
+        Anim.gameObject.SetActive(false);
+        Powder.SetActive(false);
+        colli.enabled = false;
+    }
+
+    protected override void SetCurrentState()
+    {
+        GrindNum = 0;
+        Anim.gameObject.SetActive(true);
+        Anim.SetInteger("CutNum", 0);
+        Powder.SetActive(false);
+        colli.enabled = true;
+    }
+
+    protected override void SetDoneState()
+    {
+        Anim.gameObject.SetActive(false);
+        Powder.SetActive(true);
+        colli.enabled = false;
+    }
+    private bool PestleIn;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<PestleController>())
+        {
+            PestleIn = true;
+            GrindNum++;
+            QuestManager.Instance.AddQuestCurrentAmount(goalType);
+            if (GrindNum < 3)
+            {
+                Anim.SetInteger("GrindNum", GrindNum);
+                Debug.LogError("Grinding");
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponentInParent<PestleController>())
+            PestleIn = false;
+    }
+}
---
+++ FengTienPro/Assets/Scripts/Interactable/08FeedMeds/PestleController.cs
@@ -0,0 +1,47 @@
+﻿using HTC.UnityPlugin.Vive;
+using UnityEngine;
+
+public class PestleController : IObjControllerBase, IGrabbable
+{
+    [SerializeField]    private BasicGrabbable _viveGrabFunc;
+    [SerializeField]    private HandAnim _handAnim;
+    [SerializeField]    private Quest.Name qName;
+    public BasicGrabbable viveGrabFunc => _viveGrabFunc;
+    public new HandAnim handAnim => _handAnim;
+    public GameObject Obj() => gameObject;
+    public override void Awake()
+    {
+        base.Awake();
+        ChildObj.SetActive(false);
+        hover.InteractColor = new Color(0, .74f, .74f, 1);
+        hover.hintColor = new Color(1, 0.8f, .28f, 1);
+    }
+    public override void Start()
+    {
+        viveGrabFunc.enabled = false;
+        base.SetWaitingState();
+    }
+
+    public override void OnDestroy()
+    {
+        QuestManager.Instance.GetQuestByName(qName).OnQuestChange -= OnQuestChange;
+    }
+    private void OnQuestChange(Quest.Name qName, Quest.State state)
+    {
+        switch (state)
+        {
+            case Quest.State.WAITING:
+                base.SetWaitingState();
+                hover.enabled = false;
+                break;
+            case Quest.State.CURRENT:
+                ChildObj.SetActive(true);
+                hover.enabled = true;
+                hover.ShowHintColor(GameController.Instance.mode == MainMode.Train);
+                break;
+            case Quest.State.DONE:
+                hover.enabled = false;
+                break;
+        }
+    }
+}
